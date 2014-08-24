a2d4d45a77803772832fc8db52039e3efcbaa9aa
Justin Beuck <jbeuck@yahoo.com>
2014-08-24 12:35:42 -0500

    added first talent buddy example - prediction

---
+++ talentbuddy.js
@@ -0,0 +1,5 @@
+function compute_prediction(n, w) {
+    // To print results to the standard output please use console.log()
+    // Example: console.log("Hello world!");
+    console.log(Math.floor(n * Math.pow(1.07, w)));
+}
